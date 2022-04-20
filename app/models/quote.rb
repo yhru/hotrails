256421ed75e3052981babc8211aca00b14b8ff34
Maxim Joseau <yhruvok@gmail.com>
2022-04-20 12:08:25 +0200

    feature: chapter 5

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
-  # all our model must have a name to be valid, so we add a validation
   validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) }
 end
